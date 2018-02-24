fb302f0bc4fb322b393b72a17e6255b90b56d822
Santhanu95 <36789009+Santhanu95@users.noreply.github.com>
2018-02-24 16:29:21 +0530

    loops
    java

---
+++ MulsumofNumbers.java
@@ -0,0 +1,9 @@
+class MulsumofNumbers{
+	public static void main(String args []){
+		int sum=0;
+		int i=0;
+		for(i=0; i<10;i++){
+			System.out.println("sum is="+sum);
+		}
+	}
+}
---
+++ sumofNumbers.java
@@ -0,0 +1,13 @@
+class sumofNumbers{
+	public static void main(String args []){
+		int sum=0;
+		int mul=1;
+		for(int num=1; num<=10; num++){
+			sum=sum+num;
+			mul=mul*num;
+		}
+			System.out.println("sum of number ="+sum);
+			System.out.println(Mul of number="+mul);
+	
+	}
+}
